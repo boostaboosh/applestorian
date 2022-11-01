4612e0a8db6cc3184a9402ef8ba5476deced1fdc
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2022-11-01 22:50:34 +0000

    Add pause functionality to sound button

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
 audio = new Audio("./audios/trimmed on caligraphy - One of the Greatest Speeches Ever _ Steve Jobs-[AudioTrimmer.com].mp3");
 function playMusic(){
-    if( audio.paused ) {
-    audio.play();
+    if(audio.paused) {
+        audio.play();
+    } else if(audio.play) {
+        audio.pause();
     }
 }
 
