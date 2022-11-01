c2a41dfe87f7f867e29bf778ba72b7d9847645b8
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2022-11-01 19:24:02 +0000

    Add contents and functionality to home/story page

--- javascript.js
+++ javascript.js
@@ -1,1 +1,6 @@
-alert("This is JavaScript.");
+audio = new Audio("./audios/trimmed on caligraphy - One of the Greatest Speeches Ever _ Steve Jobs-[AudioTrimmer.com].mp3");
+function playMusic(){
+    if( audio.paused ) {
+    audio.play();
+    }
+}
