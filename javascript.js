0349b3fd3b63de016e56bb66673ecd882be8ca0d
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2022-11-01 20:32:07 +0000

    Add scroll progress bar animation with bug

--- javascript.js
+++ javascript.js
@@ -4,3 +4,27 @@ function playMusic(){
     audio.play();
     }
 }
+
+let progressSection = document.querySelector(".progress-section");
+let progressBar = document.querySelector(".progress-bar");
+let progressNum = document. querySelector(".progress-num");
+
+let x, y;
+
+window.addEventListener('mousemove', (e) =>{
+    x = e.clientX;
+    y = e.clientY;
+})
+
+function updateProgressBar(){
+    progressSection.style.transform = `translate(${x}px, ${y}px)`;
+    progressBar.style.height = `${getScrollPercentage()}%`;
+    progressNum.innerText = `${Math.ceil(getScrollPercentage())}%`;
+    requestAnimationFrame(updateProgressBar);
+}
+
+function getScrollPercentage(){
+    return ((window.scrollY) / (document.body.scrollHeight - window.innerHeight) * 100);
+}
+
+updateProgressBar();
