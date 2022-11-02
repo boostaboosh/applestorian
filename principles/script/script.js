084c5c6c31eef15a65f85ac15b73bd6ec9a46234
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2022-11-02 09:43:45 +0000

    Add complete principles page

---
+++ principles/script/script.js
@@ -0,0 +1,45 @@
+function showMenu(){
+    let menu = document.getElementById('dropdown-menu');
+    if(menu.style.display == 'block'){
+        menu.style.display = 'none';
+    }else {
+        menu.style.display = 'block';                    
+    }
+}
+
+function closeMenu(){
+    let menu = document.getElementById('dropdown-menu');
+    menu.style.display = 'none';
+}
+
+document.addEventListener("click", e => {
+    const isDropdownButton = e.target.matches("[dropdown-button]");
+    if (!isDropdownButton) {
+        closeMenu();
+        return;
+    }
+})
+
+let progressSection = document.querySelector(".progress-section");
+let progressBar = document.querySelector(".progress-bar");
+let progressNum = document. querySelector(".progress-num");
+
+let x, y;
+
+window.addEventListener('mousemove', (e) =>{
+    x = e.clientX;
+    y = e.clientY;
+})
+
+function updateProgressBar(){
+    progressSection.style.transform = `translate(${x}px, ${y}px)`;
+    progressBar.style.height = `${getScrollPercentage()}%`;
+    progressNum.innerText = `${Math.ceil(getScrollPercentage())}%`;
+    requestAnimationFrame(updateProgressBar);
+}
+
+function getScrollPercentage(){
+    return ((window.scrollY) / (document.body.scrollHeight - window.innerHeight) * 100);
+}
+
+updateProgressBar();
